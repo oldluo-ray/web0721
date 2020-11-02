f1808625bad0aa03fc75a51cd6c1ba60704a910c
old-luo <luodianlei7@163.com>
2020-11-02 16:36:02 +0800

    移动端

--- MODULE/day09-redux&移动端/react/src/App.jsx
+++ MODULE/day09-redux&移动端/react/src/App.jsx
@@ -1,27 +1,12 @@
-import React, { Component } from "react";
-import {Provider} from 'react-redux'
-/*
-  1. 将class --> className
-  2. 将没有结束符标签加上结束符：input img
-  3. 将style改成{{}}
-*/
-
-// import List from "./pages/List";
-import Search from "./pages/Search";
-import WithList from './container/WithList'
-
-import store from './redux/store'
+import React, { Component } from 'react'
 
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <div className="container">
-          <Search />
-          {/* <List /> */}
-          <WithList></WithList>
-        </div>
-      </Provider>
-    );
+      <div> 
+        {/* <span>1</span> */}
+        <h1>2</h1>
+      </div>
+    )
   }
 }
